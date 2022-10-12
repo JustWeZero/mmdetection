af6f7253112e42762455d6a96080dd8efe4b3a5e
John Lu <625199292@qq.com>
2022-10-12 18:16:22 +0800

    提交了昨天晚上的结果，用原本的蒸馏文件进行改造还是不行

--- configs2/VisDrone/lka_fpn/lka_evaluation.sh
+++ configs2/VisDrone/lka_fpn/lka_evaluation.sh
@@ -18,9 +18,39 @@ python tools/test.py configs2/VisDrone/lka_fpn/${CONFIG}.py \
     --show-score-thr 0.7 --fuse-conv-bn
 
 # 知识蒸馏后的模型的可视化效果
+# 没看到结果
 export GPU=4 && LR=0.08 && CONFIG="cwd_fpnlvl_64ch_frcnn_r50_lka_fpn_frcnn_r18_lka_fpn_1x_VisDrone640"
 python tools/test.py configs2/VisDrone/lka_fpn/faster_rcnn_r18_lka_fpn_1x_VisDrone640.py \
      ../VisDrone_cache/lka_fpn/${CONFIG}/slice_640x640_lr${LR}_1x_${GPU}g/best_bbox_mAP_epoch_12_student.pth \
     --work-dir exp/${CONFIG}_lr${LR}_1x_${GPU}g \
     --show-dir ../VisDrone_cache/lka_fpn/${CONFIG}/slice_640x640_lr${LR}_1x_${GPU}g/test_result \
     --show-score-thr 0.7 --fuse-conv-bn
+# OrderedDict([('bbox_mAP', 0.176), ('bbox_mAP_50', 0.363), ('bbox_mAP_75', 0.155), ('bbox_mAP_s', 0.1), ('bbox_mAP_m', 0.263), ('bbox_mAP_l', 0.314),
+# ('bbox_mAP_copypaste', '0.176 0.363 0.155 0.100 0.263 0.314')])
+# 是不是因为show-score-thr搞的点低了，换掉试试
+export GPU=4 && LR=0.08 && CONFIG="faster_rcnn_r18_lka_fpn_1x_VisDrone640"
+python tools/test.py configs2/VisDrone/lka_fpn/faster_rcnn_r18_lka_fpn_1x_VisDrone640.py \
+     ../VisDrone_cache/lka_fpn/${CONFIG}/slice_640x640_lr${LR}_1x_${GPU}g/best_bbox_mAP_epoch_12_student.pth \
+    --work-dir exp/${CONFIG}_lr${LR}_1x_${GPU}g \
+    --show-dir ../VisDrone_cache/lka_fpn/${CONFIG}/slice_640x640_lr${LR}_1x_${GPU}g/test_result \
+    --show-score-thr 0.7 --fuse-conv-bn --eval bbox
+
+# OrderedDict([('bbox_mAP', 0.176), ('bbox_mAP_50', 0.363), ('bbox_mAP_75', 0.155), ('bbox_mAP_s', 0.1), ('bbox_mAP_m', 0.263), ('bbox_mAP_l', 0.314),
+# ('bbox_mAP_copypaste', '0.176 0.363 0.155 0.100 0.263 0.314')])
+python tools/test.py configs2/VisDrone/lka_fpn/faster_rcnn_r18_lka_fpn_1x_VisDrone640.py \
+     ../VisDrone_cache/lka_fpn/${CONFIG}/slice_640x640_lr${LR}_1x_${GPU}g/best_bbox_mAP_epoch_12_student.pth \
+    --work-dir exp/${CONFIG}_lr${LR}_1x_${GPU}g \
+    --eval bbox
+# haishi 0.176
+export GPU=4 && LR=0.08 CONFIG="faster_rcnn_r18_lka_fpn_1x_VisDrone640"
+CUDA_VISIBLE_DEVICES=0,1,2,3 PORT=10001 python tools/test.py configs2/VisDrone/lka_fpn/${CONFIG}.py \
+   ../VisDrone_cache/lka_fpn/cwd_fpnlvl_64ch_frcnn_r50_lka_fpn_frcnn_r18_lka_fpn_1x_VisDrone640/slice_640x640_lr${LR}_1x_${GPU}g/best_bbox_mAP_epoch_12_student.pth \
+  --work-dir exp/${CONFIG}_lr${LR}_1x_${GPU}g \
+  --eval bbox
+
+# yeshi 0.176……
+export GPU=4 && LR=0.08 CONFIG="test_frcnn_r18_lka_fpn_1x_VisDrone640"
+CUDA_VISIBLE_DEVICES=0,1,2,3 PORT=10001 python tools/test.py configs2/VisDrone/lka_fpn/${CONFIG}.py \
+   ../VisDrone_cache/lka_fpn/cwd_fpnlvl_64ch_frcnn_r50_lka_fpn_frcnn_r18_lka_fpn_1x_VisDrone640/slice_640x640_lr${LR}_1x_${GPU}g/best_bbox_mAP_epoch_12_student.pth \
+  --work-dir exp/${CONFIG}_lr${LR}_1x_${GPU}g \
+  --eval bbox
---
+++ configs2/VisDrone/lka_fpn/test_frcnn_r18_lka_fpn_1x_VisDrone640.py
@@ -0,0 +1,449 @@
+dataset_type = 'VisDroneCocoDataset'
+data_root = '../data/VisDrone2019/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+albu_train_transforms = [
+    dict(
+        type='ShiftScaleRotate',
+        shift_limit=0.0625,
+        scale_limit=0.0,
+        rotate_limit=45,
+        interpolation=1,
+        p=0.5),
+    dict(
+        type='RandomBrightnessContrast',
+        brightness_limit=[-0.1, 0.3],
+        contrast_limit=[0.1, 0.3],
+        p=0.4),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(
+                type='RGBShift',
+                r_shift_limit=10,
+                g_shift_limit=10,
+                b_shift_limit=10,
+                p=1.0),
+            dict(
+                type='HueSaturationValue',
+                hue_shift_limit=20,
+                sat_shift_limit=30,
+                val_shift_limit=20,
+                p=1.0)
+        ],
+        p=0.1),
+    dict(type='JpegCompression', quality_lower=85, quality_upper=95, p=0.2),
+    dict(type='ChannelShuffle', p=0.1),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(type='Blur', blur_limit=3, p=1.0),
+            dict(type='MedianBlur', blur_limit=3, p=1.0),
+            dict(type='MotionBlur', blur_limit=3, p=1.0)
+        ],
+        p=0.1)
+]
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=[(640, 512), (960, 768)], keep_ratio=True),
+    dict(
+        type='Albu',
+        transforms=[
+            dict(
+                type='ShiftScaleRotate',
+                shift_limit=0.0625,
+                scale_limit=0.0,
+                rotate_limit=45,
+                interpolation=1,
+                p=0.5),
+            dict(
+                type='RandomBrightnessContrast',
+                brightness_limit=[-0.1, 0.3],
+                contrast_limit=[0.1, 0.3],
+                p=0.4),
+            dict(
+                type='OneOf',
+                transforms=[
+                    dict(
+                        type='RGBShift',
+                        r_shift_limit=10,
+                        g_shift_limit=10,
+                        b_shift_limit=10,
+                        p=1.0),
+                    dict(
+                        type='HueSaturationValue',
+                        hue_shift_limit=20,
+                        sat_shift_limit=30,
+                        val_shift_limit=20,
+                        p=1.0)
+                ],
+                p=0.1),
+            dict(
+                type='JpegCompression',
+                quality_lower=85,
+                quality_upper=95,
+                p=0.2),
+            dict(type='ChannelShuffle', p=0.1),
+            dict(
+                type='OneOf',
+                transforms=[
+                    dict(type='Blur', blur_limit=3, p=1.0),
+                    dict(type='MedianBlur', blur_limit=3, p=1.0),
+                    dict(type='MotionBlur', blur_limit=3, p=1.0)
+                ],
+                p=0.1)
+        ],
+        bbox_params=dict(
+            type='BboxParams',
+            format='pascal_voc',
+            label_fields=['gt_labels'],
+            min_visibility=0.0,
+            filter_lost_elements=True),
+        keymap=dict(img='image', gt_bboxes='bboxes'),
+        update_pad_shape=False,
+        skip_img_without_anno=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=True),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(
+        type='Collect',
+        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_bboxes_ignore'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=[(1280, 1024), (1920, 1536)],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=2,
+    train=dict(
+        type='VisDroneCocoDataset',
+        ann_file=
+        '../data/VisDrone2019/VisDrone2019_slice640_overlap40/train_640_00625.json',
+        img_prefix=
+        '../data/VisDrone2019/VisDrone2019_slice640_overlap40/train_images_640_00625',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(
+                type='Resize',
+                img_scale=[(640, 512), (960, 768)],
+                keep_ratio=True),
+            dict(
+                type='Albu',
+                transforms=[
+                    dict(
+                        type='ShiftScaleRotate',
+                        shift_limit=0.0625,
+                        scale_limit=0.0,
+                        rotate_limit=45,
+                        interpolation=1,
+                        p=0.5),
+                    dict(
+                        type='RandomBrightnessContrast',
+                        brightness_limit=[-0.1, 0.3],
+                        contrast_limit=[0.1, 0.3],
+                        p=0.4),
+                    dict(
+                        type='OneOf',
+                        transforms=[
+                            dict(
+                                type='RGBShift',
+                                r_shift_limit=10,
+                                g_shift_limit=10,
+                                b_shift_limit=10,
+                                p=1.0),
+                            dict(
+                                type='HueSaturationValue',
+                                hue_shift_limit=20,
+                                sat_shift_limit=30,
+                                val_shift_limit=20,
+                                p=1.0)
+                        ],
+                        p=0.1),
+                    dict(
+                        type='JpegCompression',
+                        quality_lower=85,
+                        quality_upper=95,
+                        p=0.2),
+                    dict(type='ChannelShuffle', p=0.1),
+                    dict(
+                        type='OneOf',
+                        transforms=[
+                            dict(type='Blur', blur_limit=3, p=1.0),
+                            dict(type='MedianBlur', blur_limit=3, p=1.0),
+                            dict(type='MotionBlur', blur_limit=3, p=1.0)
+                        ],
+                        p=0.1)
+                ],
+                bbox_params=dict(
+                    type='BboxParams',
+                    format='pascal_voc',
+                    label_fields=['gt_labels'],
+                    min_visibility=0.0,
+                    filter_lost_elements=True),
+                keymap=dict(img='image', gt_bboxes='bboxes'),
+                update_pad_shape=False,
+                skip_img_without_anno=True),
+            dict(type='RandomFlip', flip_ratio=0.5),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(
+                type='Collect',
+                keys=['img', 'gt_bboxes', 'gt_labels', 'gt_bboxes_ignore'])
+        ]),
+    val=dict(
+        type='VisDroneCocoDataset',
+        ann_file='../data/VisDrone2019/val.json',
+        img_prefix='../data/VisDrone2019/VisDrone2019-DET-val/images',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=[(1280, 1024), (1920, 1536)],
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]),
+    test=dict(
+        type='VisDroneCocoDataset',
+        ann_file='../data/VisDrone2019/test-dev.json',
+        img_prefix='../data/VisDrone2019/VisDrone2019-DET-test-dev/images',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=[(1280, 1024), (1920, 1536)],
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]))
+optimizer = dict(type='SGD', lr=0.08, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11])
+runner = dict(type='EpochBasedRunner', max_epochs=12)
+checkpoint_config = dict(interval=1)
+log_config = dict(
+    interval=50,
+    hooks=[dict(type='TextLoggerHook'),
+           dict(type='TensorboardLoggerHook')])
+custom_hooks = [dict(type='NumClassCheckHook')]
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+train_cfg = dict(
+    rpn=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.7,
+            neg_iou_thr=0.3,
+            min_pos_iou=0.3,
+            match_low_quality=True,
+            ignore_iof_thr=-1),
+        sampler=dict(
+            type='RandomSampler',
+            num=256,
+            pos_fraction=0.5,
+            neg_pos_ub=-1,
+            add_gt_as_proposals=False),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    rpn_proposal=dict(
+        nms_across_levels=False,
+        nms_pre=2000,
+        nms_post=1000,
+        max_per_img=1000,
+        nms=dict(type='nms', iou_threshold=0.7),
+        min_bbox_size=0),
+    rcnn=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.5,
+            min_pos_iou=0.5,
+            match_low_quality=False,
+            ignore_iof_thr=-1),
+        sampler=dict(
+            type='RandomSampler',
+            num=512,
+            pos_fraction=0.25,
+            neg_pos_ub=-1,
+            add_gt_as_proposals=True),
+        pos_weight=-1,
+        debug=False))
+# test_cfg = dict(
+#     rpn=dict(
+#         nms_pre=1000,
+#         nms_post=1000,
+#         max_per_img=1000,
+#         nms=dict(type='nms', iou_threshold=0.7),
+#         min_bbox_size=0),
+#     rcnn=dict(
+#         score_thr=0.05,
+#         nms=dict(type='soft_nms', iou_threshold=0.3, min_score=0.05),
+#         max_per_img=-1))
+model = dict(
+    type='mmdet.FasterRCNN',
+    backbone=dict(
+        type='ResNet',
+        depth=18,
+        norm_eval=True,
+        norm_cfg=dict(type='BN'),
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet18'),
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        style='pytorch'),
+    neck=dict(
+        type='lka_FPN',
+        in_channels=[64, 128, 256, 512],
+        out_channels=64,
+        num_outs=5),
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=64,
+        feat_channels=64,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[2],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    roi_head=dict(
+        type='StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=64,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=64,
+            fc_out_channels=256,
+            roi_feat_size=7,
+            num_classes=10,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0.0, 0.0, 0.0, 0.0],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0))),
+    train_cfg=dict(
+        rpn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False),
+        rpn_proposal=dict(
+            nms_across_levels=False,
+            nms_pre=2000,
+            nms_post=1000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=False,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            pos_weight=-1,
+            debug=False)),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=1000,
+            nms_post=1000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            score_thr=0.05,
+            nms=dict(type='soft_nms', iou_threshold=0.3, min_score=0.05),
+            max_per_img=-1)))
+
+evaluation = dict(interval=1, metric='bbox', save_best='bbox_mAP')
+# find_unused_parameters = True
+work_dir = '../VisDrone_cache/lka_fpn/cwd_bbop_fpnlvl_frcnn_r50_lka_fpn_frcnn_r18_lka_fpn_1x_VisDrone640/slice_640x640_lr0.08_1x_4g/'
+auto_resume = False
+gpu_ids = range(0, 4)
